660c0968d3577baa4fc9048eda817aa1870630ce
Evocthings <clemens.siebers@gmail.com>
2024-07-17 00:46:31 +0200

    First complied Version

--- routes/routes.js
+++ routes/routes.js
@@ -28,5 +28,32 @@ import service from '../services/api.js'
       res.status(500).json({ error: error.message });
     }
   });
+
+  router.post('/addPurchaseOrder', async (req, res) => {
+    try {
+      const response = await service.addPurchaseOrder(req.body);
+      res.json(response.data);
+    } catch (error) {
+      res.status(500).json({ error: error.message });
+    }
+  });
+
+  router.post('/addSalesOrder', async (req, res) => {
+    try {
+      const response = await service.addSalesOrder(req.body);
+      res.json(response.data);
+    } catch (error) {
+      res.status(500).json({ error: error.message });
+    }
+  });
+
+  router.post('/updateAvailability', async (req, res) => {
+    try {
+      const response = await service.updateAvailability(req.body);
+      res.json(response.data);
+    } catch (error) {
+      res.status(500).json({ error: error.message });
+    }
+  });
   
 export default router;
--- services/api.js
+++ services/api.js
@@ -29,6 +29,33 @@ class MegaventoryService {
     return await axios.post(url, payload);
   }
 
+  async addPurchaseOrder(purchaseOrder) {
+    const url = this.buildUrl(apiUrl, '/PurchaseOrder/PurchaseOrderUpdate');
+    const payload = {
+      APIKEY: apiKey,
+      ...purchaseOrder
+    };
+    return await axios.post(url, payload);
+  }
+
+  async addSalesOrder(salesOrder) {
+    const url = this.buildUrl(apiUrl, '/SalesOrder/SalesOrderUpdate');
+    const payload = {
+      APIKEY: apiKey,
+      ...salesOrder
+    };
+    return await axios.post(url, payload);
+  }
+
+  async updateAvailability(availability) {
+    const url = this.buildUrl(apiUrl, '/InventoryLocationStock/ProductStockUpdate');
+    const payload = {
+      APIKEY: apiKey,
+      ...availability
+    };
+    return await axios.post(url, payload);
+  }
+
   buildUrl(baseUrl, endpoint) {
     return `${baseUrl}${endpoint}`;
   }
